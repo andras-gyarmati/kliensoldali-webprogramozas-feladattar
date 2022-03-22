c9a6b94862f1a8060c942c87f048ecb40eed8c1f
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2022-03-22 11:57:35 +0100

    add myplaylist-router-navigation task

---
+++ myplaylist-layout-router/src/views/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Playlists } from "./playlists/Playlists";
+import { Home } from "./home/Home";
+import { Tracks } from "./tracks/Tracks";
+import { Search } from "./search/Search";
+import { Menu } from "./menu/Menu";
+
+export function App() {
+  return (
+    <div className="ui container">
+      <Menu />
+      <Home />
+      <Playlists />
+      <Tracks />
+      <Search />
+    </div>
+  );
+}
