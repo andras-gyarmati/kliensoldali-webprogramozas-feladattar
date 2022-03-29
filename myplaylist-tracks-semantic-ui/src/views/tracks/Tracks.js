05e45123fe5a9d86418e109be920b6e09bf26dfc
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2022-03-29 10:19:43 +0200

    myplaylist-tracks-semantic-ui

---
+++ myplaylist-tracks-semantic-ui/src/views/tracks/Tracks.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { exampleTracks } from "../../domain/track";
+import { TrackForm } from "./TrackForm";
+import { Track } from "./Track";
+
+export function Tracks() {
+  const tracks = exampleTracks;
+
+  return (
+    <>
+      <div className="ui container">
+        <button href="#" className="ui right floated green button" id="newModal">
+          <i className="plus icon"></i>
+          New track
+        </button>
+        <h1>Tracks</h1>
+        <table className="ui celled striped table">
+          <thead>
+            <tr>
+              <th>Artist</th>
+              <th>Title</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tracks.map((track) => (
+              <Track key={track.id} track={track} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <TrackForm />
+    </>
+  );
+}
